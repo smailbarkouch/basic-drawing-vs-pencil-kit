16b27f070f5b6d1f34d0e92d225d8d34301fcb04
Smail Barkouch <smailbarkouch1@gmail.com>
2024-09-04 22:08:13 -0400

    Initial Commit

---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by Smail Barkouch on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
